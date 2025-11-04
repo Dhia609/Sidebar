9db39fa7159216b4af7a0d9b1637125efa1d2a82
Dhia609 <dhiamoulazem@gmail.com>
2025-11-04 22:17:35 +0100

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,72 @@
+       <a type="button" class="sidebar-toggle" id="sidebar-toggle">
+
+     
+  <svg fill="#000000" width="64px" height="64px" viewBox="0 0 24 24" id="menu-alt-2" data-name="Line Color" xmlns="http://www.w3.org/2000/svg" class="icon line-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M21,3H14v7h7ZM10,14H3v7h7Z" style="fill: none; stroke: #2ca9bc; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="primary" d="M10,10H3V3h7Zm11,4H14v7h7Z" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></g></svg>
+      </a> 
+<div class="sidebar-container">
+     
+  <div class="sidebar">
+    
+<ul class="sidebar-list">
+    <li class="sidebar-list-item item-1">
+        <a class="mainlist" href="/"><span>Tableau de Board</span></a>
+    </li>
+    <li class="sidebar-list-item item-2">
+        <a class="mainlist" href="#"><span>Gestion de l'Entreprise</span></a>
+        <ul class="sublist">
+            <li><a href="account.php"><span>Données de l'Entreprise</span></a></li>
+            <li><a href="clients.php"><span>Clients</span></a></li>
+            <li><a href="suppliers.php"><span>Fournisseurs</span></a></li>
+            <li><a href="staff.php"><span>Gestion des Employés</span></a></li>
+            <li><a href="staffGroups.php"><span>Groupes</span></a></li>
+            <li><a href="staffGroups.php"><span>Gestion des offres d'emploi</span></a></li>
+            <li><a href="staffGroups.php"><span>Demandes d'emploi</span></a></li>
+
+        </ul>
+    </li>
+    <li class="sidebar-list-item item-3">
+        <a class="mainlist" href="#"><span>Gestion des Ventes</span></a>
+        <ul class="sublist">
+            <li><a href="productsList.php"><span>Gestion de Stock</span></a></li>
+            <li><a href="orders"><span>Commandes & Factures</span></a></li>
+            <li><a href="livraisons"><span>Livraisons</span></a></li>
+            <li><a href="sales"><span>Ventes</span></a></li>
+            <li><a href="returnsList.php"><span>Retours & Échange</span></a></li>
+            <li><a href="caisse"><span>Caisse</span></a></li>
+            <li><a href="Installments.php"><span>Paiements Échelonnés</span></a></li>
+            <li><a href="quotes.php"><span>Devis</span></a></li>
+        </ul>
+    </li>
+    <li class="sidebar-list-item item-4">
+        <a class="mainlist" href="#"><span>Gestion des Projets</span></a>
+        <ul class="sublist">
+            <li><a href="myProjects.php"><span>Activités & Projets</span></a></li>
+            <li><a href="bills.php"><span>Factures à Payer</span></a></li>
+            <li><a href="notes.php"><span>Rapports</span></a></li>
+        </ul>
+    </li>
+    
+       <li class="sidebar-list-item item-5">
+                <a class="mainlist" href="myDocs.php"><span>Gestion des Documents</span></a>
+    </li>
+    
+   <li class="sidebar-list-item item-6">
+                <a class="mainlist" href=""><span>Gestion Web</span></a>
+    </li>
+
+<li class="sidebar-list-item item-7">
+        <a class="mainlist" href="#"><span>Autres</span></a>
+        <ul class="sublist">
+            <li><a href="Notes.php"><span>Remarques</span></a></li>
+            <li><a href="calendar.php"><span>Calendrier</span></a></li>
+        </ul>
+    </li>
+
+    <li class="sidebar-list-item item-8">
+        <a class="mainlist" href="SeDéconnecter"><span>Guide d'Utilisation</span></a>
+    </li>
+</ul>
+
+
+  </div>
+  </div>
